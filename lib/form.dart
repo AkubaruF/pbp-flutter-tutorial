2fe9a73df4b40c84f9b061f3040a2914254aa870
akbar.firdaus <akbar.firdaus@ui.ac.id>
2022-11-10 14:59:24 +0700

    Change name form position

--- lib/form.dart
+++ lib/form.dart
@@ -30,6 +30,41 @@ class _MyFormPageState extends State<MyFormPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Padding(
+              // Menggunakan padding sebesar 8 pixels
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Contoh: Pak Dengklek",
+                  labelText: "Nama Lengkap",
+                  // Menambahkan icon agar lebih intuitif
+                  icon: const Icon(Icons.people),
+                  // Menambahkan circular border agar lebih rapi
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                // Menambahkan behavior saat nama diketik
+                onChanged: (String? value) {
+                  setState(() {
+                    _namaLengkap = value!;
+                  });
+                },
+                // Menambahkan behavior saat data disimpan
+                onSaved: (String? value) {
+                  setState(() {
+                    _namaLengkap = value!;
+                  });
+                },
+                // Validator sebagai validasi form
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nama lengkap tidak boleh kosong!';
+                  }
+                  return null;
+                },
+              ),
+            ),
             const ListTile(
               leading: Icon(Icons.school),
               title: Text("Jenjang"),
@@ -174,41 +209,6 @@ class _MyFormPageState extends State<MyFormPage> {
               child: const Text(
                 "Simpan",
                 style: TextStyle(color: Colors.white),
-              ),
-            ),
-            Padding(
-              // Menggunakan padding sebesar 8 pixels
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Contoh: Pak Dengklek",
-                  labelText: "Nama Lengkap",
-                  // Menambahkan icon agar lebih intuitif
-                  icon: const Icon(Icons.people),
-                  // Menambahkan circular border agar lebih rapi
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                ),
-                // Menambahkan behavior saat nama diketik
-                onChanged: (String? value) {
-                  setState(() {
-                    _namaLengkap = value!;
-                  });
-                },
-                // Menambahkan behavior saat data disimpan
-                onSaved: (String? value) {
-                  setState(() {
-                    _namaLengkap = value!;
-                  });
-                },
-                // Validator sebagai validasi form
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Nama lengkap tidak boleh kosong!';
-                  }
-                  return null;
-                },
               ),
             ),
           ],
